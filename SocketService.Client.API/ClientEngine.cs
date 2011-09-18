3f593fb79892e8d86e1316a938671bd1cdeef8c8
scottmccain <scott.mccain@gmail.com>
2011-09-17 21:25:35 -0700

    Added client engine.

---
+++ SocketService.Client.API/ClientEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocketService.Client.API
+{
+    public class ClientEngine
+    {
+        public void Connect()
+        {
+            // TODO: read info from configuration
+        }
+    
+    }
+}
