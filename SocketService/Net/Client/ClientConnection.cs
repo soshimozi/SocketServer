066f958e5cbce29f7543f33b580c66ac0aec5d41
scottmccain <scott.mccain@gmail.com>
2011-10-31 21:34:06 -0700

    blah blah blah blah

---
+++ SocketService/Net/Client/ClientConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SocketService.Core.Crypto;
+
+namespace SocketService.Net.Client
+{
+    public class ClientConnection //: Connection
+    {
+
+        /// <summary>
+        /// Gets or sets the client id.
+        /// </summary>
+        /// <value>
+        /// The client id.
+        /// </value>
+        public Guid ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the remote public key.
+        /// </summary>
+        /// <value>
+        /// The remote public key.
+        /// </value>
+        public DiffieHellmanKey RemotePublicKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the provider.
+        /// </summary>
+        /// <value>
+        /// The provider.
+        /// </value>
+        public DiffieHellmanProvider SecureKeyProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state of the connection.
+        /// </summary>
+        /// <value>
+        /// The state of the connection.
+        /// </value>
+        public ConnectionState ConnectionState { get; set; }
+    }
+
+    public enum ConnectionState
+    {
+        NegotiateKeyPair,
+        Connected
+    }
+}
