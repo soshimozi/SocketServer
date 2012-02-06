f5dd2fbecb5957cb1f5e83521abd1b4dfd440e5c
scottmccain <scott.mccain@gmail.com>
2012-02-05 16:03:20 -0800

    Added new crypto based on AES 256 key.

---
+++ SocketService.Crypto/RijndaelCrypto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SocketServer.Crypto
+{
+    public class RijndaelCrypto
+    {
+        private const int DefaultKeySize = 256;
+
+        public string Encrypt(string plainText, string password)
+        {
+            var plainBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(
+                Encrypt(
+                    plainBytes,
+                    SymmetricAlgorithmFactory.GetRijndaelManaged(password, DefaultKeySize)
+                )
+            );
+        }
+
+        public string Decrypt(string cipherText, string password)
+        {
+            var encryptedBytes = Convert.FromBase64String(cipherText);
+            return Encoding.UTF8.GetString(
+                Decrypt(
+                    encryptedBytes, 
+                    SymmetricAlgorithmFactory.GetRijndaelManaged(password, DefaultKeySize)
+                )
+            );
+
+        }
+
+        public static byte[] Encrypt(byte[] plainBytes, RijndaelManaged rijndaelManaged)
+        {
+            return rijndaelManaged.CreateEncryptor()
+                .TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+        }
+
+        public static byte[] Decrypt(byte[] encryptedData, RijndaelManaged rijndaelManaged)
+        {
+            return rijndaelManaged.CreateDecryptor()
+                .TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+        }
+    }
+}
---
+++ SocketService.Crypto/SymmetricAlgorithmFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SocketServer.Crypto
+{
+    class SymmetricAlgorithmFactory
+    {
+        public static RijndaelManaged GetRijndaelManaged(String secretKey, int keySize)
+        {
+            var keyBytes = new byte[keySize / 8];
+            var secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+            Array.Copy(secretKeyBytes, keyBytes, Math.Min(keyBytes.Length, secretKeyBytes.Length));
+            return new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7,
+                KeySize = keySize,
+                BlockSize = keySize,
+                Key = keyBytes,
+                IV = keyBytes
+            };
+        }
+    }
+}
